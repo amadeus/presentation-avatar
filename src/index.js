ca069bb5de8eb02ccec586823d445ad8d0668267
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2019-08-27 16:22:24 -0700

    Presentation preps

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 // @flow strict
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Helmet from 'react-helmet';
 import './index.css';
 import App from './App';
 
 const el = document.getElementById('root');
-el != null && ReactDOM.render(<App />, el);
+el != null &&
+  ReactDOM.render(
+    <>
+      <Helmet>
+        <title>Building The New Avatar</title>
+      </Helmet>
+      <App />
+    </>,
+    el
+  );
