9df6dbf2074022b34ca7d213885459d594ec2cd9
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2019-08-27 21:48:41 -0700

    Allow ColorWrapper to be paused

--- src/common/ColorWrapper.js
+++ src/common/ColorWrapper.js
@@ -1,5 +1,5 @@
 // @flow strict
-import React, {useEffect, type Node} from 'react';
+import React, {useState, useEffect, type Node} from 'react';
 import {useSpring, animated} from 'react-spring';
 import styles from './ColorWrapper.module.css';
 
@@ -18,12 +18,23 @@ type ColorWrapperProps = {|
 
 function ColorWrapper({children, opacity = 0.2}: ColorWrapperProps) {
   const [style, updateSpring] = useSpring(() => ({backgroundColor: getHSLAFromXY(0, 0, opacity)}));
+  const [paused, setPause] = useState(false);
   useEffect(() => {
+    const handlePress = (event: KeyboardEvent) => {
+      if (event.key === 'p') {
+        setPause(b => !b);
+      }
+    };
+    document.addEventListener('keypress', handlePress);
+    return () => void document.addEventListener('keypress', handlePress);
+  }, []);
+  useEffect(() => {
+    if (paused) return;
     const handleMouseMove = ({pageX, pageY}: MouseEvent) =>
       updateSpring({backgroundColor: getHSLAFromXY(pageX, pageY, opacity)});
     document.addEventListener('mousemove', handleMouseMove);
     return () => document.removeEventListener('mousemove', handleMouseMove);
-  }, [updateSpring, opacity]);
+  }, [updateSpring, opacity, paused]);
   return (
     <animated.div style={style} className={styles.container}>
       {children}
